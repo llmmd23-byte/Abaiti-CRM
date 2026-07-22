ee632d600451f959458baf95f1ced287ad042fa3
sl0ld <mr.fks.r0@gmail.com>
2026-07-22 19:02:13 +0300

    Expand roasting zone frame

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -3664,7 +3664,7 @@ const FLOOR_MAP_ZONES = [
   { key: "all", labelAr: "كل الأقسام", labelEn: "All zones", left: 0, top: 0, width: 0, height: 0 },
   { key: "prefunction", labelAr: "قاعة ما قبل الفعالية", labelEn: "Pre-Function Hall", left: 3.8, top: 4.6, width: 82.8, height: 9.2 },
   { key: "traders", labelAr: "سوق التجار", labelEn: "Traders Market", left: 3.8, top: 17.2, width: 41.8, height: 53.6 },
-  { key: "roasting", labelAr: "منطقة التحميص", labelEn: "Roasting Area", left: 53.2, top: 17.2, width: 30.8, height: 53.6 },
+  { key: "roasting", labelAr: "منطقة التحميص", labelEn: "Roasting Area", left: 50.8, top: 17.2, width: 34.6, height: 70.2 },
   { key: "farmers", labelAr: "سوق مزارعين البن", labelEn: "Coffee Farmers Market", left: 87.4, top: 17.2, width: 7.8, height: 46.4 },
   { key: "innovation", labelAr: "ساحة الابتكار", labelEn: "Innovation Area", left: 53.2, top: 74.6, width: 41.8, height: 21.6 },
   { key: "stage", labelAr: "الساحة والمسرح", labelEn: "Plaza & Stage", left: 3.8, top: 75.6, width: 41.8, height: 21.0 },
