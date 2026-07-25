26ffe8a74ca5f149e8b077204b9eee43b698bcf0
sl0ld <mr.fks.r0@gmail.com>
2026-07-25 10:15:32 +0300

    Restyle admin top navigation

--- next.config.mjs
+++ next.config.mjs
@@ -4,6 +4,7 @@ const withNextIntl = createNextIntlPlugin();
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  allowedDevOrigins: ["127.0.0.1"],
   async headers() {
     return [
       {
