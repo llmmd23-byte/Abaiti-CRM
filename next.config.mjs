eff50e4b0b08b11a8d3c2a62953cf33f0ff7fbee
sl0ld <mr.fks.r0@gmail.com>
2026-07-26 13:21:57 +0300

    Commit all pending changes

--- next.config.mjs
+++ next.config.mjs
@@ -5,19 +5,6 @@ const withNextIntl = createNextIntlPlugin();
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   allowedDevOrigins: ["127.0.0.1"],
-  async headers() {
-    return [
-      {
-        source: "/api/:path*",
-        headers: [
-          {
-            key: "Content-Type",
-            value: "application/json; charset=utf-8",
-          },
-        ],
-      },
-    ];
-  },
   images: {
     unoptimized: true
   }
