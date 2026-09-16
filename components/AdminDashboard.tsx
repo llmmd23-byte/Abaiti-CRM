2a16189c3dbd633785714e0f68933da55354c3dd
sl0ld <mr.fks.r0@gmail.com>
2026-09-16 18:15:14 +0300

    Prevent booth group overlap

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -4747,18 +4747,18 @@ export const NEW_BOOTH_MAP_WIDTH = 100;
 export const NEW_BOOTH_MAP_HEIGHT = 100;
 
 const newDGroups = [
-  {ids: [35, 36, 37], left: 8, top: 14},
-  {ids: [38, 39, 40], left: 21, top: 14},
-  {ids: [41, 42, 43], left: 34, top: 14},
-  {ids: [44, 45, 46], left: 47, top: 14},
-  {ids: [47, 48, 49], left: 60, top: 14},
-  {ids: [50, 51], left: 73, top: 14},
-  {ids: [18, 19, 20, 1, 2, 3], left: 8, top: 32},
-  {ids: [21, 22, 23, 4, 5, 6], left: 21, top: 32},
-  {ids: [24, 25, 26, 7, 8, 9], left: 34, top: 32},
-  {ids: [27, 28, 29, 10, 11, 12], left: 47, top: 32},
-  {ids: [30, 31, 32, 13, 14, 15], left: 60, top: 32},
-  {ids: [33, 34, 16, 17], left: 73, top: 32},
+  {ids: [35, 36, 37], left: 7, top: 14},
+  {ids: [38, 39, 40], left: 22, top: 14},
+  {ids: [41, 42, 43], left: 37, top: 14},
+  {ids: [44, 45, 46], left: 52, top: 14},
+  {ids: [47, 48, 49], left: 67, top: 14},
+  {ids: [50, 51], left: 82, top: 14},
+  {ids: [18, 19, 20, 1, 2, 3], left: 7, top: 32},
+  {ids: [21, 22, 23, 4, 5, 6], left: 22, top: 32},
+  {ids: [24, 25, 26, 7, 8, 9], left: 37, top: 32},
+  {ids: [27, 28, 29, 10, 11, 12], left: 52, top: 32},
+  {ids: [30, 31, 32, 13, 14, 15], left: 67, top: 32},
+  {ids: [33, 34, 16, 17], left: 82, top: 32},
 ] as const;
 
 export const NEW_EMERGENCY_EXITS = [8, 25, 42, 59, 76, 91] as const;
@@ -4781,9 +4781,9 @@ export const NEW_BOOTH_LAYOUT = [
   ...newDGroups.flatMap((group) =>
     group.ids.map((id, index) => ({
       id: `D${id}`,
-      left: group.left + (index % 3) * 5.2,
+      left: group.left + (index % 3) * 4.7,
       top: group.top + (index >= 3 ? 7 : 0),
-      width: 4.7,
+      width: 4.2,
       height: 5.8,
     })),
   ),
