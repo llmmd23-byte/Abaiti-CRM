d346e4a064e5866ea49e86dba57a3b092c5e5692
sl0ld <mr.fks.r0@gmail.com>
2026-09-18 11:44:41 +0300

    Move D booth section left visibly

--- components/AdminDashboard.tsx
+++ components/AdminDashboard.tsx
@@ -4747,18 +4747,18 @@ export const NEW_BOOTH_MAP_WIDTH = 100;
 export const NEW_BOOTH_MAP_HEIGHT = 100;
 
 const newDGroups = [
-  {ids: [35, 36, 37], left: 0.5, top: 14},
-  {ids: [38, 39, 40], left: 17.5, top: 14},
-  {ids: [41, 42, 43], left: 34.5, top: 14},
-  {ids: [44, 45, 46], left: 51.5, top: 14},
-  {ids: [47, 48, 49], left: 68.5, top: 14},
-  {ids: [50, 51], left: 85.5, top: 14},
-  {ids: [18, 19, 20, 1, 2, 3], left: 0.5, top: 27},
-  {ids: [21, 22, 23, 4, 5, 6], left: 17.5, top: 27},
-  {ids: [24, 25, 26, 7, 8, 9], left: 34.5, top: 27},
-  {ids: [27, 28, 29, 10, 11, 12], left: 51.5, top: 27},
-  {ids: [30, 31, 32, 13, 14, 15], left: 68.5, top: 27},
-  {ids: [33, 34, 16, 17], left: 85.5, top: 27},
+  {ids: [35, 36, 37], left: -1.5, top: 14},
+  {ids: [38, 39, 40], left: 15.5, top: 14},
+  {ids: [41, 42, 43], left: 32.5, top: 14},
+  {ids: [44, 45, 46], left: 49.5, top: 14},
+  {ids: [47, 48, 49], left: 66.5, top: 14},
+  {ids: [50, 51], left: 83.5, top: 14},
+  {ids: [18, 19, 20, 1, 2, 3], left: -1.5, top: 27},
+  {ids: [21, 22, 23, 4, 5, 6], left: 15.5, top: 27},
+  {ids: [24, 25, 26, 7, 8, 9], left: 32.5, top: 27},
+  {ids: [27, 28, 29, 10, 11, 12], left: 49.5, top: 27},
+  {ids: [30, 31, 32, 13, 14, 15], left: 66.5, top: 27},
+  {ids: [33, 34, 16, 17], left: 83.5, top: 27},
 ] as const;
 
 export const NEW_BOOTH_LAYOUT = [
@@ -4794,18 +4794,18 @@ export const NEW_BOOTH_LAYOUT = [
 
 export const NEW_BOOTH_GROUPS = [
   ...[
-    {left: 0.1, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
-    {left: 17.1, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
-    {left: 34.1, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
-    {left: 51.1, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
-    {left: 68.1, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
-    {left: 85.1, top: 13.6, width: 9.2, height: 6.6, kind: "d-top"},
-    {left: 0.1, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
-    {left: 17.1, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
-    {left: 34.1, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
-    {left: 51.1, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
-    {left: 68.1, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
-    {left: 85.1, top: 26.6, width: 9.2, height: 12.4, kind: "d-middle"},
+    {left: -1.9, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
+    {left: 15.1, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
+    {left: 32.1, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
+    {left: 49.1, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
+    {left: 66.1, top: 13.6, width: 13.4, height: 6.6, kind: "d-top"},
+    {left: 83.1, top: 13.6, width: 9.2, height: 6.6, kind: "d-top"},
+    {left: -1.9, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
+    {left: 15.1, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
+    {left: 32.1, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
+    {left: 49.1, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
+    {left: 66.1, top: 26.6, width: 13.4, height: 12.4, kind: "d-middle"},
+    {left: 83.1, top: 26.6, width: 9.2, height: 12.4, kind: "d-middle"},
   ] as const,
   ...[
     {left: 7, top: 49, width: 14.5, height: 17},
